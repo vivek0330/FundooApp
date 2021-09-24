1ba250f02a38be216a0949da299d67270c85a02f
vivek <vivek.iitd93@gmail.com>
2021-09-24 18:02:19 +0530

    Feat: validation test case done

--- test/registration-test.js
+++ test/registration-test.js
@@ -1,44 +1,44 @@
-const server = require("../server");
+// const server = require("../server");
 
-//Require the dependencies
-const chai = require("chai");
-const chaiHttp = require("chai-http");
-const userDB = require("./user.json");
+// //Require the dependencies
+// const chai = require("chai");
+// const chaiHttp = require("chai-http");
+// const userDB = require("./user.json");
 
-chai.should();
-chai.use(chaiHttp);
+// chai.should();
+// chai.use(chaiHttp);
 
-// test for registration Api
-describe("registration API", () => {
-  it("User Registration", (done) => {
-    let registrationDetails = userDB.user.register;
-    console.log(registrationDetails);
-    chai
-      .request(server)
-      .post("/register")
-      .send(registrationDetails)
-      .end((err, res) => {
-        res.should.have.status(201);
-        done();
-      });
-  });
-});
+// // test for registration Api
+// describe("registration API", () => {
+//   it("User Registration", (done) => {
+//     let registrationDetails = userDB.user.register;
+//     console.log(registrationDetails);
+//     chai
+//       .request(server)
+//       .post("/register")
+//       .send(registrationDetails)
+//       .end((err, res) => {
+//         res.should.have.status(201);
+//         done();
+//       });
+//   });
+// });
 
-// test for login Api
-describe("signin API", () => {
-  it("User successfully logged In", (done) => {
-    let registrationDB = {
-      email: "Lalit@gmail.com",
-      password: "Lalit@@",
-    };
-    console.log(registrationDB);
-    chai
-      .request(server)
-      .post("/signin")
-      .send(registrationDB)
-      .end((err, res) => {
-        res.should.have.status(200);
-        done();
-      });
-  });
-});
+// // test for login Api
+// describe("signin API", () => {
+//   it("User successfully logged In", (done) => {
+//     let registrationDB = {
+//       email: "Lalit@gmail.com",
+//       password: "Lalit@@",
+//     };
+//     console.log(registrationDB);
+//     chai
+//       .request(server)
+//       .post("/signin")
+//       .send(registrationDB)
+//       .end((err, res) => {
+//         res.should.have.status(200);
+//         done();
+//       });
+//   });
+// });
---
+++ test/validate-test.js
@@ -0,0 +1,20 @@
+const assert = require("chai").assert;
+const userValidate = require("../helpers/joiValidation.js");
+const test = require("./user.json");
+
+describe("user data Validation", () => {
+  it("for given valid data should return null error", () => {
+    const user = test.user.registerAuth;
+    assert.equal(userValidate.registraionAuth.validate(user).error, null);
+  });
+
+  it("for given valid data should return null error", () => {
+    const user = test.user.registerAuth1;
+    assert.equal(userValidate.registraionAuth.validate(user).error, null);
+  });
+
+  it("for given Invalid data should return null data", () => {
+    const user = test.user.registerunAuth;
+    assert.equal(userValidate.registraionAuth.validate(user).data, null);
+  });
+});
